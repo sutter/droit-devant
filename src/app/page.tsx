2dc42e33e1b41bd3e5278b2e52fd97da49b25c4f
Laurent Sutterlity <laurent@sutterlity.studio>
2025-11-27 21:05:06 +0100

    Add logo image and update home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,16 @@
+import Image from "next/image"
+
 export default function Home() {
   return (
-    <div className="flex min-h-screen items-center justify-center">
+    <div className="flex min-h-screen items-center justify-center bg-linear-to-b from-[#0a011c] to-[#9f21d6]">
       <main className="p-12 text-center">
-        <h1 className="text-4xl font-bold uppercase text-primary">
-          Droit devant
-        </h1>
-        <p className="text-primary">Trace ta ligne, tient ton cap</p>
+        <Image
+          src="/images/logo-80.png"
+          alt="Droit devant"
+          width={300}
+          height={300}
+          className="rounded-xl"
+        />
       </main>
     </div>
   )
